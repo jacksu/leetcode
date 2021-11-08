ca0e3ae4cba2798b899d011e5230334e6a47a0c2
jacksu <371387455@qq.com>
2021-11-08 23:34:34 +0800

    add wordBreak

--- WordBreak.py
+++ WordBreak.py
@@ -1,3 +1,4 @@
+from locale import str
 from typing import List
 
 
@@ -15,3 +16,32 @@ class WordBreak:
             return res
 
         return back_track(s)
+
+    def wordBreak2(self, s: str, wordDict: List[str]) -> List[str]:
+        import functools
+        @functools.lru_cache(None)
+        def backtrack(index: int) -> List[List[str]]:
+            if index == len(s):
+                return [[]]
+            ans = list()
+            for i in range(index + 1, len(s) + 1):
+                word = s[index:i]
+                if word in wordSet:
+                    nextWordBreaks = backtrack(i)
+                    for nextWordBreak in nextWordBreaks:
+                        ans.append(nextWordBreak.copy() + [word])
+            return ans
+
+        wordSet = set(wordDict)
+        breakList = backtrack(0)
+        return [" ".join(words[::-1]) for words in breakList]
+
+
+if __name__ == '__main__':
+    s = "catsanddog"
+    print(s[::-1])
+    wordDict = ["cat", "cats", "and", "sand", "dog"]
+    print([" ".join(words[::-1]) for  words in wordDict])
+    egg = WordBreak()
+    str = egg.wordBreak2(s, wordDict)
+    print(str)
